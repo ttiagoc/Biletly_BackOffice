85e704f45c0d4956ad936f8a51bc56a481591d11
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-22 11:32:11 -0300

    meti template

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,38 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../logo.png'
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+ 
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <img alt="Brand" src={logo} height={60} width={60} />
+      
+    </div>
+
+   
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        
+      </ul>
+     
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
   )
 }
